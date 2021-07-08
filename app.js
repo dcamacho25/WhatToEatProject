04188f359e95c97194ea60ef0f433f288a846504
Daniel Camacho <daniel_camacho_1@yahoo.com>
2021-07-07 21:24:05 -0400

    add js to create element below the GO button once clicked

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 let selectTime = document.getElementById("selectTime");
 let selectMeal = document.getElementById("selectMeal");
+const recipeButton = document.getElementById("recipeButton");
 
-function displayRecipe(){
-    let displayTime = selectTime.options[selectTime.selectedIndex].text;
-    let displayMeal = selectMeal.options[selectMeal.selectedIndex].text;
-    console.log(displayTime, displayMeal);
-};
+
+// Event Listener to receive time and meal
+// recipeButton.addEventListener('click', () =>{
+//     let displayTime = selectTime.options[selectTime.selectedIndex].text;
+//     let displayMeal = selectMeal.options[selectMeal.selectedIndex].text;
+//     console.log(displayTime, displayMeal);
+// });
+
+// Add Recipe below the GO buton
+recipeButton.addEventListener('click', () =>{
+    let article = document.getElementById('recipeContainer');
+    let div = document.createElement('div');
+    div.textContent = "hello world";
+    const cls = ["border", "border-primary", "mt-5"];
+    div.classList.add(...cls);
+    article.appendChild(div);
+});
