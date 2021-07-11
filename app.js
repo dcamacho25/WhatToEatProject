ce131a26f3fc1fe800f1e79e12fd26be709071de
Daniel Camacho <daniel_camacho_1@yahoo.com>
2021-07-10 22:22:02 -0400

    remove extra brunch object in recipes.json and clean up fetch request code

--- app.js
+++ app.js
@@ -2,26 +2,28 @@ let selectTime = document.getElementById("selectTime");
 let selectMeal = document.getElementById("selectMeal");
 const recipeButton = document.getElementById("recipeButton");
 
-
+// Fetch data for recipes
 fetch('https://dcamacho25.github.io/WhatToEatProject/recipes.json')
   .then(response => response.json())
-  .then(data => console.log(data));
-
-
+  .then(filterList);
 
 // Event Listener to receive time and meal
-// recipeButton.addEventListener('click', () =>{
-//     let displayTime = selectTime.options[selectTime.selectedIndex].text;
-//     let displayMeal = selectMeal.options[selectMeal.selectedIndex].text;
-//     console.log(displayTime, displayMeal);
-// });
+recipeButton.addEventListener('click', () =>{
+    let displayTime = selectTime.options[selectTime.selectedIndex].text;
+    let displayMeal = selectMeal.options[selectMeal.selectedIndex].text;
+    console.log(displayTime, displayMeal);
+});
 
 // Add Recipe below the GO buton
 recipeButton.addEventListener('click', () =>{
-    let article = document.getElementById('recipeContainer');
-    let div = document.createElement('div');
-    div.textContent = "Hello World";
-    const cls = ["border", "border-primary", "mt-5"];
-    div.classList.add(...cls);
-    article.appendChild(div);
+    const recipeBox = document.getElementById('recipeContainer');
+    recipeBox.innerHTML = `<h1> Title <h1>`
 });
+
+function filterList(recipeList) {
+  for(let i= 0; i<recipeList.length; i +=1){
+    if (recipeList[i].category === "brunch"){
+      console.log(recipeList[i]);
+    }
+  }
+};
