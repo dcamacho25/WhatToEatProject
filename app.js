71c102ceb33e64011b6928694a8b6ff491702b3e
Daniel Camacho <daniel_camacho_1@yahoo.com>
2021-07-13 19:44:28 -0400

    clean up json data and display recipes based on meal option

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 let selectTime = document.getElementById("selectTime");
 let selectMeal = document.getElementById("selectMeal");
-const recipeButton = document.getElementById("recipeButton");
+const recipeButton = document.getElementById("recipeButton"); 
 
+function filterList(recipeList) {
+  const mealChoice = selectMeal.value.toLowerCase();
+  recipeList.forEach(list => {
+    if(list.category === mealChoice) {
+      console.log(list.title);
+    }
+  });
+};
+
+// Event Listener to receive time and meal
+recipeButton.addEventListener('click', () =>{
 // Fetch data for recipes
 fetch('https://dcamacho25.github.io/WhatToEatProject/recipes.json')
   .then(response => response.json())
   .then(filterList);
-
-// Event Listener to receive time and meal
-recipeButton.addEventListener('click', () =>{
-    let displayTime = selectTime.options[selectTime.selectedIndex].text;
-    let displayMeal = selectMeal.options[selectMeal.selectedIndex].text;
-    console.log(displayTime, displayMeal);
 });
 
 // Add Recipe below the GO buton
-recipeButton.addEventListener('click', () =>{
-    const recipeBox = document.getElementById('recipeContainer');
-    recipeBox.innerHTML = `<h1> Title <h1>`
-});
+// recipeButton.addEventListener('click', () =>{
+//     const recipeBox = document.getElementById('recipeContainer');
+//     recipeBox.innerHTML = `<h1> Title <h1>`
+// });
 
-function filterList(recipeList) {
-  for(let i= 0; i<recipeList.length; i +=1){
-    if (recipeList[i].category === "brunch"){
-      console.log(recipeList[i]);
-    }
-  }
-};
