91438233b482cfc9e18474604941d92be1f71c6e
Daniel Camacho <daniel_camacho_1@yahoo.com>
2021-07-28 02:33:41 -0400

    display multiple recipes in 2 columns with js and adjust styles for conversion section

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 let selectTime = document.getElementById('selectTime');
 let selectMeal = document.getElementById('selectMeal');
+
 let cupAmount = document.getElementById('cupInput');
 let tbspAmount = document.getElementById('tbspOutput');
+
 const recipeButton = document.getElementById('recipeButton');
 const recipeBox = document.getElementById('recipeContainer');
 const classesArray = ['card', 'border', 'border-2', 'text-start', 'rounded', 'bg-dark', 'm-2'];
+const resultClasses = ['border', 'border-2', 'bg-light', 'text-dark'];
 
 // dividing 1 by 0.0625 converts cups to tablespoons
 function calCupsToTbsp(){
+  tbspAmount.innerHTML= '';
   let tbspInCup = cupAmount.value / 0.0625;
-  tbspAmount.innerText = tbspInCup;
+  let tbspResult = document.createElement('div')
+  tbspResult.textContent = '= ' + tbspInCup + ' tablespoons';
+  tbspAmount.appendChild(tbspResult);
+  tbspResult.classList.add(resultClasses);
 }
 
 function createCard(title, classes){
   let card = document.createElement('div');
   let cardBody = document.createElement('div');
   let cardTitle = document.createElement('div');
-
+  let col = document.createElement('div');
+  
+  col.classList.add('col');
   card.classList.add(...classes);
   cardBody.classList.add('card-body');
   cardTitle.classList.add('card-title');
 
   cardTitle.textContent = title;
+  col.appendChild(card);
   card.appendChild(cardBody);
   cardBody.appendChild(cardTitle);
-  return card;
+  return col;
 
 };
 
